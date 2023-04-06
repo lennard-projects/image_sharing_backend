4fcd84058ed4d90be9f0d430074f556daecd18c7
Lennard Articona <gototodile1212@gmail.com>
2023-04-06 13:31:37 +0800

    initial commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 
-app.use(cors({origin: "http://localhost:3000"}))
+app.use(cors({origin: "https://image-sharing-application.netlify.app"}))
 
 app.use('/user', userRoutes)
 app.use('/posts', postRoutes)
